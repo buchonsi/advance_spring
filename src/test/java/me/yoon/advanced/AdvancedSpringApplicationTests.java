e662b4b5f73755bb22dc237c296b7090bd694f35
buchonsi <buchonsi@naver.com>
2024-01-10 00:06:46 +0900

    Log trace prototype

---
+++ src/test/java/me/yoon/advanced/AdvancedSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package me.yoon.advanced;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
