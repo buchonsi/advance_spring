4707b6056ffb8c777473230638762f4d3249185d
buchonsi <buchonsi@naver.com>
2024-01-10 12:46:17 +0900

    V3 - 동시성 문제를 ThreadLocal을 이용하여 해결
    * ThreadLocal은 Thread가 종료되기 전에 remove 해주어야 한다. > 같은 Thread를 사용할 때 remove가 제거 되어있지 않으면 ThreadLocal의 이전 값이 노출될 수 있다

---
+++ src/test/java/me/yoon/advanced/trace/threadlocal/ThreadLocalServiceTest.java
@@ -0,0 +1,68 @@
+package me.yoon.advanced.trace.threadlocal;
+
+import lombok.extern.slf4j.Slf4j;
+import me.yoon.advanced.trace.threadlocal.code.FieldService;
+import me.yoon.advanced.trace.threadlocal.code.ThreadLocalService;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ThreadLocalServiceTest {
+
+    private final ThreadLocalService fieldService = new ThreadLocalService();
+
+    @Test
+    void field() {
+        log.info("main start");
+
+        Runnable userA = () -> {
+            fieldService.logic("userA");
+        };
+
+        Runnable userB = () -> {
+            fieldService.logic("userB");
+        };
+
+        Thread threadA = new Thread(userA);
+        threadA.setName("thread-A");
+        Thread threadB = new Thread(userB);
+        threadB.setName("thread-B");
+
+        threadA.start();
+        sleep(2000);       //동시성 문제 발생 X
+        threadB.start();
+        sleep(3000);        //메인 쓰레드 종료 대기
+        log.info("main exit");
+    }
+
+    @Test
+    void field_error() {
+        log.info("main start");
+
+        Runnable userA = () -> {
+            fieldService.logic("userA");
+        };
+
+        Runnable userB = () -> {
+            fieldService.logic("userB");
+        };
+
+        Thread threadA = new Thread(userA);
+        threadA.setName("thread-A");
+        Thread threadB = new Thread(userB);
+        threadB.setName("thread-B");
+
+        threadA.start();
+        sleep(100);       //동시성 문제 발생 O
+        threadB.start();
+        sleep(3000);        //메인 쓰레드 종료 대기
+        log.info("main exit");
+    }
+
+    private void sleep(int mills) {
+        try {
+            Thread.sleep(mills);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
