ba4482fdc6ba857f6ec52aa36452a317a2ca7987
buchonsi <buchonsi@naver.com>
2024-01-12 16:20:58 +0900

    프록시 패턴 - 인터페이스가 있는 구체 클래스 - 스프링 빈으로 수동 등록

--- src/main/java/me/yoon/advanced/AdvancedSpringApplication.java
+++ src/main/java/me/yoon/advanced/AdvancedSpringApplication.java
@@ -1,9 +1,12 @@
 package me.yoon.advanced;
 
+import me.yoon.advanced.proxy.config.AppV1Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+@Import(AppV1Config.class)
+@SpringBootApplication(scanBasePackages = "me.yoon.advanced.proxy.app")      //지정해 주지 않으면 현재 위치 하위의 모든 클래스를 componentScan을 한다. 현재는 중복된 이름이 빈으로 등록되어있으므로 범위를 축소해서 수동 등록한다.
 public class AdvancedSpringApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/me/yoon/advanced/proxy/app/v1/OrderRepositoryV1.java
@@ -0,0 +1,5 @@
+package me.yoon.advanced.proxy.app.v1;
+
+public interface OrderRepositoryV1 {
+    void save(String itemId);
+}
